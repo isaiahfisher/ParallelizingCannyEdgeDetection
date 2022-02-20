8fd4f4a79a1499073b833a2d0032f03b124308af
Mark Trovinger <mark.trovinger@gmail.com>
2022-02-20 09:02:43 -0500

    started c++ implementation

---
+++ serialImplementations/traditionalCannyAlgorithm.cpp
@@ -0,0 +1,30 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int* edgeDetection(Mat image, float sigma, float low, float high){
+    // Step 1: Gaussian
+    // check to make sure we can open the image
+    if(image.empty()){
+        cout << "Could not find the image!" << endl;
+        return -1;
+    }
+
+    Mat blurred_image;
+    GaussianBlur(image, blurred_image, Size(5,5), 0);
+
+    // Step 2: Gradient Calculation
+    // Apply Sobel filter
+    Mat xGradient, yGradient;
+    Sobel(blurred_image, xGradient, CV_64F, 1, 0, ksize=3);
+    Sobel(blurred_image, yGradient, CV_64F, 0, 1, ksize=3);
+
+    // convert to polar coordinates
+    Mat magnitute, angle;
+    cartToPolar(xGradient, yGradient, magnitude, angle, angleInDegrees = true);
+    
+    // non-Maximum suppression
+    // loop through all pixels in the image
+}
