2513fc5c8a1b9ca2efbf88adb398a6e1268514d7
Mark Trovinger <mark.trovinger@gmail.com>
2022-02-21 09:06:58 -0500

    started step 3

--- serialImplementations/traditionalCannyAlgorithm.cpp
+++ serialImplementations/traditionalCannyAlgorithm.cpp
@@ -4,7 +4,7 @@
 using namespace cv;
 using namespace std;
 
-int* edgeDetection(Mat image, float sigma, float low, float high){
+Mat* edgeDetection(Mat image, float sigma, float low, float high){
     // Step 1: Gaussian
     // check to make sure we can open the image
     if(image.empty()){
@@ -33,13 +33,28 @@ int* edgeDetection(Mat image, float sigma, float low, float high){
     int channels = image.channels();
     int nRows = image.rows;
     int nCols = image.cols * channels();
-    
+    int firstNeighborX, firstNeighborY, secondNeighborX, secondNeighborY = 0;
     int i, j, currAngle;
     uchar* p;
     for(i = 0; i < nRows; ++i){
         p = image<ptr>char(i);
         for(j = 0; j < nCols; ++j){
-            
+            int currAngle = angle[p];
+            if(currAngle > 180){
+                currAngle -= 180;
+            }
+            if(currAngle <= 22.5 || currAngle > 157.5){
+                firstNeighborX = p - 1;
+                firstNeighborY = j;
+                secondNeighborX = p + 1;
+                secondNeighborY = j;
+            }
+            else if(currAngle > 22.5 && currAngle <= 67.5){
+                firstNeighborX = p - 1;
+                firstNeighborY = j - 1;
+                secondNeighborX = p + 1;
+                secondNeighborY = j;
+            }
         }
     }
 }
