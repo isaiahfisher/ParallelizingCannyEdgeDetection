8f9429102e1e80ccf7d205e94067fc4447add309
isaiah fisher <isaiahfisher@gmail.com>
2022-03-03 12:14:48 -0500

    my attempt at canny using C++

--- serialImplementations/traditionalCannyAlgorithm.cpp
+++ serialImplementations/traditionalCannyAlgorithm.cpp
@@ -1,112 +1,301 @@
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/opencv.hpp>
-#include <iostream>
+#include "stdafx.h"
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+#include<string>
+#include<iostream> 
 
 using namespace cv;
 using namespace std;
 
-int edgeDetection(Mat image, float sigma, float low, float high){
-    // Step 1: Gaussian
-    // check to make sure we can open the image
-    Mat detectedEdges;
-    if(image.empty()){
-        cout << "Could not find the image!" << endl;
-        return -1;
-    }
 
-    Mat blurred_image;
-    GaussianBlur(image, blurred_image, Size(5,5), 0);
-
-    // Step 2: Gradient Calculation
-    // Apply Sobel filter
-    Mat xGradient, yGradient;
-    Sobel(blurred_image, xGradient, CV_64F, 1, 0, 3);
-    Sobel(blurred_image, yGradient, CV_64F, 0, 1, 3);
-
-    // convert to polar coordinates
-    Mat magnitude, angle;
-    cartToPolar(xGradient, yGradient, magnitude, angle, true);
-    cout << magnitude << endl;
+//Our canny edge detection implementation
+void EdgeDetection(Mat input, Mat output, low, high, sigma)
+{
+    //declare Mat fields that will hold different image manipulation stages
+    Mat imgGrayscale, imgBlurred, xGradient, yGradient, magnitude, angle;
     
-    // non-Maximum suppression
-    // loop through all pixels in the image
-    // according to https://docs.opencv.org/2.4/doc/tutorials/core/how_to_scan_images/how_to_scan_images.html#howtoscanimagesopencv
-    // using classic C pointers is the most efficient
-    
-    int channels = image.channels();
-    int nRows = image.rows;
-    int nCols = image.cols * channels;
-    int firstNeighborX, firstNeighborY, secondNeighborX, secondNeighborY = 0;
-    int i, j, currAngle;
-    uchar* p;
-    /*
-    for(i = 0; i < nRows; ++i){
-        p = image.ptr<uchar>(i);
-        for(j = 0; j < nCols; ++j){
-            int currAngle = angle[j][i];
-            if(currAngle > 180){
+    //get the grayscale image then blur it using a 5 x 5 gaussian kernal
+    cvtColor(input, imgGrayscale, CV_BGR2GRAY);
+    GaussianBlur(imgGrayscale, imgBlurred, Size(5, 5), sigma); 
+    //get the gradients of the image using Sobel kernal
+    Sobel(imgBlurred, xGradient, CV_64F, 1, 0, ksize=3);
+    Sobel(imgBlurred, yGradient, CV_64F, 0, 1, ksize=3);
+
+    //grab polar coordinates from the gradients
+    CartToPolar(xGradient, yGradient, magnitude, angle, true);
+
+    //loop through all pixels in the image
+    for (int i = 0; i < imgBlurred.cols; i++)
+    {
+        for (int j = 0; j < imgBlurred.rows; j++)
+        {
+
+            uchar currAngle = angle.at<uchar>(j, i);
+            int firstNeighborX;
+            int firstNeighborY;
+            int secondNeighborX;
+            int secondNeighborY;
+            if (currAngle > 180)
+            {
                 currAngle -= 180;
             }
-            if(currAngle <= 22.5 || currAngle > 157.5){
-                firstNeighborX = p - 1;
-                firstNeighborY = j;
-                secondNeighborX = p + 1;
-                secondNeighborY = j;
-            }
-            else if(currAngle > 22.5 && currAngle <= 67.5){
-                firstNeighborX = p - 1;
+            if (currAngle <= 22.5 || currAngle > 157.5)
+            {
+               firstNeighborX = i - 1;
+               firstNeighborY = j;
+            } else if (currAngle > 22.5 && currAngle <= 67.5)
+            {
+                firstNeighborX = i - 1;
                 firstNeighborY = j - 1;
-                secondNeighborX = p + 1;
-                secondNeighborY = j;
-            }
-            else if(currAngle > 67.5 && currAngle <= 112.5){
-                firstNeighborX = p - 1;
+                secondNeighborX = i + 1;
+                secondNeighborY = j + 1;
+            } else if (currAngle > 67.5 && currAngle <= 112.5)
+            {
+                firstNeighborX = i - 1;
                 firstNeighborY = j + 1;
-                secondNeighborX = p + 1;
+                secondNeighborX = i;
+                secondNeighborY = j - 1;
+            } else if (currAngle > 112.5 && currAngle <= 157.5)
+            {
+                firstNeighborX = i
+                firstNeighborY = j + 1;
+                secondNeighborX = i;
                 secondNeighborY = j - 1;
             }
-            else if(currAngle > 112.5 && currAngle <= 157.5){
-                firstNeighborX = p;
-                firstNeighborY = j + 1;
-                secondNeighborX = p;
-                secondNeighborY = y - 1;
+            
+            //check each pixel against its neighbor
+            if (firstNeighborX >= 0 && firstNeighborY >= 0)
+            {
+                if (input.cols > firstNeighborX && input.rows > firstNeighborY )
+                {
+                    if (magnitude.at<uchar>(j,i) < magnitude.at<uchar>(firstNeighborY, firstNeighborX))
+                    {
+                        magnitude.at<uchar>(j,i) = 0;
+                    }
+                }
             }
-
-            if(firstNeighborX >= 0 && firstNeighborY >= 0){
-                if(nCols > firstNeighborX && nRows > firstNeighborY){
-                    if(magnitude[j, p] < magnitude[firstNeighborX, firstNeighborY]){
-                        magnitude[j, p] = 0;
+            if (secondNeighborX >= 0 && secondNeighborY >= 0)
+            {
+                if (input.cols > secondNeighborX && input.rows > secondNeighborY )
+                {
+                    if (magnitude.at<uchar>(j,i) < magnitude.at<uchar>(secondNeighborY, secondNeighborX))
+                    {
+                        magnitude.at<uchar>(j,i) = 0;
                     }
-                    }
+                }
             }
         }
-        
-    } */
-    return 0;
+    }
+
+    //Hysteresis thresholding
+    //loop through all pixels in the image and discard 0s
+    //100 = weak threshold 255 = strong threshhold
+    for (int i = 0; i < imgBlurred.cols; i++)
+    {
+        for (int j = 0; j < imgBlurred.rows; j++)
+        {
+            uchar currMagnitude = magnitude.at<uchar>(j,i);
+
+            if (currMagnitude < low)
+            {
+                magnitude.at<uchar>(j,i) = 0;
+            } else if (currMagnitude >=100 && currMagnitude < high)
+            {
+                magnitude.at<uchar>(j,i) = 100;
+            } else
+            {
+                magnitude.at<uchar>(j,i) = 255;
+            }
+        }
+    }
+
+    //starting in top left determine if a weak threshold should be kept
+    //or discarded
+    Mat top_left = magnitude.clone();
+    for (int i = 0; i < imgBlurred.cols; i++)
+    {
+        for (int j = 0; j < imgBlurred.rows; j++)
+        {
+            if (top_left.at<uchar>(j,i) == 100)
+            {
+                try
+                {
+                    if (top_left.at<uchar>(j+1, i) == 255
+                            || top_left.at<uchar>(j-1,i) == 255
+                            || top_left.at<uchar>(j+1, i-1) == 255
+                            || top_left.at<uchar>(j+1, i+1) == 255
+                            || top_left.at<uchar>(j-1, i-1) == 255
+                            || top_left.at<uchar>(j-1, i+1) == 255
+                            || top_left.at<uchar>(j, i-1) == 255
+                            || top_left.at<uchar(j, i+1) == 255)
+                    {
+                        top_left.at<uchar>(j,i) = 255;
+                    } else
+                    {
+                        top_left.at<uchar>(j,i) = 0;
+                    }
+                } catch (...) //catches all exceptions
+                {
+                    //equivalent to python pass I believe
+                    ;
+                }
+            }
+        }
+    }
+
+    //starting from top right instead
+    Mat top_right = magnitude.clone();
+    for (int i = imgBlurred.cols - 1; i > 0; i--)
+    {
+        for (int j = 0; j < imgBlurred.rows - 1; j++)
+        {
+            if (top_right.at<uchar>(j,i) == 100)
+            {
+                try
+                {
+                    if (top_right.at<uchar>(j+1, i) == 255
+                            || top_right.at<uchar>(j-1,i) == 255
+                            || top_right.at<uchar>(j+1, i-1) == 255
+                            || top_right.at<uchar>(j+1, i+1) == 255
+                            || top_right.at<uchar>(j-1, i-1) == 255
+                            || top_right.at<uchar>(j-1, i+1) == 255
+                            || top_right.at<uchar>(j, i-1) == 255
+                            || top_right.at<uchar(j, i+1) == 255)
+                    {
+                        top_right.at<uchar>(j,i) = 255;
+                    } else
+                    {
+                        top_right.at<uchar>(j,i) = 0;
+                    }
+                } catch (...)
+                {
+                    ;
+                }
+            }
+        }
+    }
+
+    //starting from bottom left instead
+    Mat bottom_left = magnitude.clone();
+    for (int i = 0; i < imgBlurred.cols - 1; i++)
+    {
+        for (int j = imgBlurred.rows - 1; j > 0; j--)
+        {
+            if (bottom_left.at<uchar>(j,i) == 100)
+            {
+                try
+                {
+                    if (bottom_left.at<uchar>(j+1, i) == 255
+                            || bottom_left.at<uchar>(j-1,i) == 255
+                            || bottom_left.at<uchar>(j+1, i-1) == 255
+                            || bottom_left.at<uchar>(j+1, i+1) == 255
+                            || bottom_left.at<uchar>(j-1, i-1) == 255
+                            || bottom_left.at<uchar>(j-1, i+1) == 255
+                            || bottom_left.at<uchar>(j, i-1) == 255
+                            || bottom_left.at<uchar(j, i+1) == 255)
+                    {
+                        bottom_left.at<uchar>(j,i) = 255;
+                    } else
+                    {
+                        bottom_left.at<uchar>(j,i) = 0;
+                    }
+                } catch (...)
+                {
+                    ;
+                }
+            }
+        }
+    }
+
+    //starting from bottom right instead
+    Mat bottom_right = magnitude.clone();
+    for (int i = imgBlurred.cols - 1; i > 0; i--)
+    {
+        for (int j = imgBlurred.rows - 1; j > 0; j--)
+        {
+            if (bottom_right.at<uchar>(j,i) == 100)
+            {
+                try
+                {
+                    if (bottom_right.at<uchar>(j+1, i) == 255
+                            || bottom_right.at<uchar>(j-1,i) == 255
+                            || bottom_right.at<uchar>(j+1, i-1) == 255
+                            || bottom_right.at<uchar>(j+1, i+1) == 255
+                            || bottom_right.at<uchar>(j-1, i-1) == 255
+                            || bottom_right.at<uchar>(j-1, i+1) == 255
+                            || bottom_right.at<uchar>(j, i-1) == 255
+                            || bottom_right.at<uchar(j, i+1) == 255)
+                    {
+                        bottom_right.at<uchar>(j,i) = 255;
+                    } else
+                    {
+                        bottom_right.at<uchar>(j,i) = 0;
+                    }
+                } catch (...)
+                {
+                    ;
+                }
+            }
+        }
+    }
+
+    //combine all versions and make all edges value 255
+    Mat final_magnitude = top_left + top_right + bottom_left + bottom_right;
+    for (int i = 0; i < imgBlurred.cols; i++)
+    {
+        for(int j = 0; i < imgBlurred.rows; j++)
+        {
+            if (final_magnitude.at<uchar>(j,i) > 0)
+            {
+                final_magnitude.at<uchar(j,i) = 255;
+            }
+        }
+    }
+
+    output = final_magnitude.clone();
+
 }
 
-int main(int argc, char* argv[]){
-    // read image from disk, using cmd line
-    // parameters, using tutorial parameters for now
-    string image_path = samples::findFile("starry_night.jpg");
-    Mat img = imread(image_path, IMREAD_COLOR);
-    if(img.empty())
-    {
-        std::cout << "Could not read the image: " << image_path << std::endl;
-        return 1;
-    }
-    edgeDetection(img, 0.0, 0.0, 0.0);
-    imshow("Display window", img);
-    int k = waitKey(0); // Wait for a keystroke in the window
-    if(k == 's')
-    {
-        imwrite("starry_night.png", img);
-    }
-    return 0;
-    // call Canny's algorithm, and output the 
-    // edged image to disk
+int main()
+{
 
-    
+    //variables to hold various image states during the edge detection process
+    Mat imgOriginal; 
+    Mat imgCanny; 
+
+    //Prompting user input
+    cout << "Please enter an image filename(string): ";
+    string img_addr;
+    cin >> img_addr;
+    cout << "Please enter a sigma value(double): ";
+    double sigma;
+    cin >> sigma;
+
+    //let the user know their selection and open the image
+    cout << "Searching for " + img_addr << endl;
+    imgOriginal = imread(img_addr);
+
+    //make sure the image exists
+    if (imgOriginal.empty()) 
+    {
+        cout << "error: image not read from file\n\n";
+        return(0); 
+    }
+    //let the user know that edge detection has begun
+    char sigmaStr[10];
+    sprintf(sigmaStr, "%d", sigma);
+    cout << "Detecting lines with a sigma value of: " + sigmaStr << endl;
+
+    EdgeDetection(imgOriginal, imgCanny, 100, 200, sigma);
+
+    //CV_WINDOW_AUTOSIZE will fix the window to image size
+    namedWindow("imgOriginal", CV_WINDOW_AUTOSIZE);        
+    namedWindow("imgCanny", CV_WINDOW_AUTOSIZE);
+
+    //Show windows
+    imshow("imgOriginal", imgOriginal);        
+    imshow("imgCanny", imgCanny);
+
+    waitKey(0);
+    return 0;
 }
